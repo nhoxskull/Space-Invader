dd64a6b0dfcad491c1a1f20c27c3f69244d965e6
nhoxskull <32362756+nhoxskull@users.noreply.github.com>
2017-09-28 10:08:39 -0400

    Update Aliens.java

--- Aliens.java
+++ Aliens.java
@@ -5,9 +5,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
-/**
- * Created by hoang on 7/14/2017.
- */
 public class Aliens extends GameObject {
 
     public Aliens(Image img){
